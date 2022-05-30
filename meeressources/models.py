fb8292e09beff04017174c383d9b17eeda4df3b3
CHOUMMIKH Meriam <meriamchoummikh2002@gmail.com>
2022-05-30 22:57:52 +0200

    Création de la base de données

--- meeressources/models.py
+++ meeressources/models.py
@@ -1,3 +1,94 @@
+from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
 
 # Create your models here.
+
+class Utilisateur(models.Model):
+    id = models.BigAutoField(primary_key = True)
+    nom = models.fields.CharField(max_length=100)
+    prénom = models.fields.CharField(max_length=100)
+    email = models.fields.EmailField(unique=True)
+    mot_de_passe = models.fields.CharField(max_length = 20)
+    commentaire = models.fields.CharField(max_length = 150)
+
+class Statut(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    nom = models.fields.CharField(max_length=100)
+    commentaire = models.fields.CharField(max_length = 150)
+
+class Utilisateur_Statut(models.Model):
+    id_utilisateur = models.ForeignKey(Utilisateur, on_delete=models.CASCADE, primary_key = True)
+    id_statut = models.ForeignKey(Statut, on_delete=models.CASCADE, primary_key = True)
+    date_début = models.DateField()
+    date_fin = models.DateField()
+    commentaire = models.fields.CharField(max_length = 150)
+
+class Responsabilité(models.Model):
+    id_utilisateur = models.ForeignKey(Utilisateur, on_delete=models.CASCADE, primary_key = True)
+    resp_pièce = models.BooleanField()
+    resp_clé = models.BooleanField()
+    resp_machine = models.BooleanField()
+    resp_ressource_info = models.BooleanField()
+    commentaire = models.fields.CharField(max_length = 150)
+
+class Machine(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    commentaire = models.fields.CharField(max_length = 150)
+
+class Utilisateur_Machine(models.Model):
+    id_utilisateur = models.ForeignKey(Utilisateur, on_delete=models.CASCADE, primary_key = True)
+    id_machine = models.ForeignKey(Machine, on_delete=models.CASCADE, primary_key = True)
+    date_début = models.DateField()
+    date_fin = models.DateField()
+    commentaire = models.fields.CharField(max_length = 150)
+
+class Ressource_Info(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    commentaire = models.fields.CharField(max_length = 150)
+
+class Utilisateur_Ressource_Info(models.Model):
+    id_utilisateur = models.ForeignKey(Utilisateur, on_delete=models.CASCADE, primary_key = True)
+    id_ressource = models.ForeignKey(Ressource_Info, on_delete=models.CASCADE, primary_key = True)
+    date_début = models.DateField()
+    date_fin = models.DateField()
+    commentaire = models.fields.CharField(max_length = 150)
+
+class Rôle(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    nom = models.CharField(max_length = 100)
+    commentaire = models.fields.CharField(max_length = 150)
+
+class Utilisateur_Rôle(models.Model):
+    id_utilisateur = models.ForeignKey(Utilisateur, on_delete=models.CASCADE, primary_key = True)
+    id_rôle = models.ForeignKey(Rôle, on_delete=models.CASCADE, primary_key = True)
+    date_début = models.DateField()
+    date_fin = models.DateField()
+    commentaire = models.fields.CharField(max_length = 150)
+
+class Pièce(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    nom_salle = models.CharField(max_length = 100)
+    commentaire = models.fields.CharField(max_length = 150)
+
+class Utilisateur_Pièce(models.Model):
+    id_utilisateur = models.ForeignKey(Utilisateur, on_delete=models.CASCADE, primary_key = True)
+    id_pièce = models.ForeignKey(Pièce, on_delete=models.CASCADE, primary_key = True)
+    date_début = models.DateField()
+    date_fin = models.DateField()
+    commentaire = models.fields.CharField(max_length = 150)
+
+class Clé(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    commentaire = models.fields.CharField(max_length = 150)
+
+class Utilisateur_Clé(models.Model):
+    id_utilisateur = models.ForeignKey(Utilisateur, on_delete=models.CASCADE, primary_key = True)
+    id_clef = models.ForeignKey(Clé, on_delete=models.CASCADE, primary_key = True)
+    date_début = models.DateField()
+    date_fin = models.DateField()
+    commentaire = models.fields.CharField(max_length = 150)
+
+class Pièce_Clé(models.Model):
+    id_pièce = models.ForeignKey(Pièce, on_delete=models.CASCADE, primary_key = True)
+    id_clé = models.ForeignKey(Clé, on_delete=models.CASCADE, primary_key = True)
+    commentaire = models.fields.CharField(max_length = 150)
