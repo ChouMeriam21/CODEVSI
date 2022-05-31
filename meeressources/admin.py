b845e42969228a0dc6d5b63d9db92042f958adde
Merlin Duval <merlin.duval@imt-atlantique.net>
2022-05-31 18:52:52 +0200

    site d'administration ok

--- meeressources/admin.py
+++ meeressources/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from meeressources.models import *
+
+admin.site.register(Utilisateur)
+admin.site.register(Statut)
+admin.site.register(Utilisateur_Statut)
+admin.site.register(Responsabilité)
+admin.site.register(Machine)
+admin.site.register(Utilisateur_Machine)
+admin.site.register(Ressource_Info)
+admin.site.register(Utilisateur_Ressource_Info)
+admin.site.register(Rôle)
+admin.site.register(Utilisateur_Rôle)
+admin.site.register(Pièce)
+admin.site.register(Utilisateur_Pièce)
+admin.site.register(Clé)
+admin.site.register(Utilisateur_Clé)
+admin.site.register(Pièce_Clé)
--- meeressources/models.py
+++ meeressources/models.py
@@ -11,17 +11,30 @@ class Utilisateur(models.Model):
     mot_de_passe = models.fields.CharField(max_length = 20)
     commentaire = models.fields.CharField(max_length = 150)
 
+    def __str__(self): #Fonction qui sert retourne une chaîne de caractère pour identifier l'instance de la classe
+        return self.nom + " " + self.prénom
+
 class Statut(models.Model):
     id = models.BigAutoField(primary_key=True)
     nom = models.fields.CharField(max_length=100)
     commentaire = models.fields.CharField(max_length = 150)
 
+    def __str__(self):
+        return self.nom
+
 class Utilisateur_Statut(models.Model):
     id_utilisateur = models.ForeignKey(Utilisateur, on_delete=models.CASCADE, primary_key = True)
-    id_statut = models.ForeignKey(Statut, on_delete=models.CASCADE, primary_key = True)
+    id_statut = models.ForeignKey(Statut, on_delete=models.CASCADE)
     date_début = models.DateField()
     date_fin = models.DateField()
     commentaire = models.fields.CharField(max_length = 150)
+
+    class Meta :
+        unique_together = ('id_utilisateur', 'id_statut')
+    
+    def __str__(self):
+        return self.commentaire
+
 
 class Responsabilité(models.Model):
     id_utilisateur = models.ForeignKey(Utilisateur, on_delete=models.CASCADE, primary_key = True)
@@ -31,64 +44,120 @@ class Responsabilité(models.Model):
     resp_ressource_info = models.BooleanField()
     commentaire = models.fields.CharField(max_length = 150)
 
+    def __str__(self):
+        return str(self.id_utilisateur)
+
 class Machine(models.Model):
     id = models.BigAutoField(primary_key=True)
     commentaire = models.fields.CharField(max_length = 150)
 
+    def __str__(self):
+        return self.commentaire
+
 class Utilisateur_Machine(models.Model):
     id_utilisateur = models.ForeignKey(Utilisateur, on_delete=models.CASCADE, primary_key = True)
-    id_machine = models.ForeignKey(Machine, on_delete=models.CASCADE, primary_key = True)
+    id_machine = models.ForeignKey(Machine, on_delete=models.CASCADE)
     date_début = models.DateField()
     date_fin = models.DateField()
     commentaire = models.fields.CharField(max_length = 150)
+
+    class Meta :
+        unique_together = ('id_utilisateur', 'id_machine')
+    
+    def __str__(self):
+        return self.commentaire
 
 class Ressource_Info(models.Model):
     id = models.BigAutoField(primary_key=True)
     commentaire = models.fields.CharField(max_length = 150)
 
+    def __str__(self):
+        return self.commentaire
+
 class Utilisateur_Ressource_Info(models.Model):
     id_utilisateur = models.ForeignKey(Utilisateur, on_delete=models.CASCADE, primary_key = True)
-    id_ressource = models.ForeignKey(Ressource_Info, on_delete=models.CASCADE, primary_key = True)
+    id_ressource = models.ForeignKey(Ressource_Info, on_delete=models.CASCADE)
     date_début = models.DateField()
     date_fin = models.DateField()
     commentaire = models.fields.CharField(max_length = 150)
+
+    class Meta :
+        unique_together = ('id_utilisateur', 'id_ressource')
+    
+    def __str__(self):
+        return self.commentaire
 
 class Rôle(models.Model):
     id = models.BigAutoField(primary_key=True)
     nom = models.CharField(max_length = 100)
     commentaire = models.fields.CharField(max_length = 150)
 
+    def __str__(self):
+        return self.nom
+
 class Utilisateur_Rôle(models.Model):
     id_utilisateur = models.ForeignKey(Utilisateur, on_delete=models.CASCADE, primary_key = True)
-    id_rôle = models.ForeignKey(Rôle, on_delete=models.CASCADE, primary_key = True)
+    id_rôle = models.ForeignKey(Rôle, on_delete=models.CASCADE)
     date_début = models.DateField()
     date_fin = models.DateField()
     commentaire = models.fields.CharField(max_length = 150)
+
+    class Meta :
+        unique_together = ('id_utilisateur', 'id_rôle')
+    
+    def __str__(self):
+        return self.commentaire
 
 class Pièce(models.Model):
     id = models.BigAutoField(primary_key=True)
     nom_salle = models.CharField(max_length = 100)
     commentaire = models.fields.CharField(max_length = 150)
 
+    def __str__(self):
+        return self.nom_salle
+
 class Utilisateur_Pièce(models.Model):
     id_utilisateur = models.ForeignKey(Utilisateur, on_delete=models.CASCADE, primary_key = True)
-    id_pièce = models.ForeignKey(Pièce, on_delete=models.CASCADE, primary_key = True)
+    id_pièce = models.ForeignKey(Pièce, on_delete=models.CASCADE)
     date_début = models.DateField()
     date_fin = models.DateField()
     commentaire = models.fields.CharField(max_length = 150)
+
+    class Meta :
+        unique_together = ('id_utilisateur', 'id_pièce')
+    
+    def __str__(self):
+        return self.commentaire
+
 
 class Clé(models.Model):
     id = models.BigAutoField(primary_key=True)
     commentaire = models.fields.CharField(max_length = 150)
 
+    def __str__(self):
+        return self.commentaire
+
 class Utilisateur_Clé(models.Model):
     id_utilisateur = models.ForeignKey(Utilisateur, on_delete=models.CASCADE, primary_key = True)
-    id_clef = models.ForeignKey(Clé, on_delete=models.CASCADE, primary_key = True)
+    id_clef = models.ForeignKey(Clé, on_delete=models.CASCADE)
     date_début = models.DateField()
     date_fin = models.DateField()
     commentaire = models.fields.CharField(max_length = 150)
 
+    class Meta :
+        unique_together = ('id_utilisateur', 'id_clef')
+
+    
+    def __str__(self):
+        return self.commentaire
+
 class Pièce_Clé(models.Model):
     id_pièce = models.ForeignKey(Pièce, on_delete=models.CASCADE, primary_key = True)
-    id_clé = models.ForeignKey(Clé, on_delete=models.CASCADE, primary_key = True)
+    id_clé = models.ForeignKey(Clé, on_delete=models.CASCADE)
     commentaire = models.fields.CharField(max_length = 150)
+
+    class Meta :
+        unique_together = ('id_pièce', 'id_clé')
+
+    def __str__(self):
+        return self.commentaire
