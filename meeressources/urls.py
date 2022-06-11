6c5106c0977ead2a79ad3d9ed1e63e8ca89630b9
CHOUMMIKH Meriam <meriamchoummikh2002@gmail.com>
2022-06-11 14:16:25 +0200

    pages html

--- meeressources/urls.py
+++ meeressources/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('fiche/',views.page, name='page'),
+    path('membresMEE/',views.PageMembres, name='membresMEE'),
+    path('modifier/',views.Modifier, name='modifier'),
+    path('inscription/',views.Inscription, name='inscription'),
 ]
--- meeressources/views.py
+++ meeressources/views.py
@@ -4,3 +4,11 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'PageAccueil.html')
+def page(request):
+    return render(request, 'page.html')
+def PageMembres(request):
+    return render(request, 'PageMembres.html')
+def Modifier(request):
+    return render(request, 'Modifier.html')
+def Inscription(request):
+    return render(request, 'Inscription.html')
